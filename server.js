d26323efbc32af04a992d34f99b4962d5a2bcc27
Cristina <cristina.rotaru@futurelearn.com>
2016-02-27 12:28:19 +0000

    Create handler and write response

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = 'Hurray NodeGirls!';
+
+function handler(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message);
+  response.end();
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function() {
   console.log('Welcome to the SERVER. Listening on port 3000. Ready to roll!')
